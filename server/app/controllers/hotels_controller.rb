aa8686e530b0475e34075bf2406e16b1db7c69b8
Kimokeo Rabang <kimokerava@gmail.com>
2015-02-19 16:19:03 -0500

    Worked on controllers, added create and update for nested routes

--- server/app/controllers/hotels_controller.rb
+++ server/app/controllers/hotels_controller.rb
@@ -1,6 +1,42 @@
 class HotelsController < ApplicationController
 
-  private
+  def index
+    @state = State.find params[:state_id]
+    @hotels = @state.hotels
+    respond_to do |format|
+      format.json { render json: @state.hotels.to_json }
+    end
+  end
+
+  def create
+    @state = State.find params[:state_id]
+    @hotel = @state.hotels.new hotel_params
+    if @hotel.save
+      respond_to do |format|
+        format.json { render json: @state.hotels.to_json }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: @state.hotels.errors.full_messages, status: 422}
+      end
+    end
+  end
+
+  def update
+    @hotel = Hotel.find params[:id]
+    if @hotel.update_attributes hotel_params
+      respond_to do |format|
+        format.json { render json: @hotel.to_json }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: @hotel.errors.full_messages, status: 422}
+      end
+    end
+  end
+  
+
+private
   def hotel_params
     params.require(:hotel).permit(
       :name,
--- server/app/controllers/restaurants_controller.rb
+++ server/app/controllers/restaurants_controller.rb
@@ -1,5 +1,41 @@
 class RestaurantsController < ApplicationController
 
+  def index
+    @state = State.find params[:state_id]
+    @restaurants = @state.restaurants
+    respond_to do |format|
+      format.json { render json: @state.restaurants.to_json }
+    end
+  end
+
+  def create
+    @state = State.find params[:state_id]
+    @restaurant = @state.restaurants.new restaurant_params
+    if @restaurant.save
+      respond_to do |format|
+        format.json { render json: @state.restaurants.to_json }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: @state.restaurants.errors.full_messages, status: 422}
+      end
+    end
+  end
+
+  def update
+    @restaurant = Restaurant.find params[:id]
+    if @restaurant.update_attributes restaurant_params
+      respond_to do |format|
+        format.json { render json: @restaurant.to_json }
+      end
+    else
+      respond_to do |format|
+        format.json { render json: @restaurant.errors.full_messages, status: 422}
+      end
+    end
+  end
+  
+
 
   private
   def restaurant_params
