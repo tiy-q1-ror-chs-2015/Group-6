dfd36c34486f9acf21444dbb507fe4c044b6caa9
William Holt <wtholt27@gmail.com>
2015-02-20 11:56:38 -0500

    added slug to states, ran migration

--- server/app/controllers/states_controller.rb
+++ server/app/controllers/states_controller.rb
@@ -1,13 +1,25 @@
 class StatesController < ApplicationController
   def index
-    @states = States.all
+    @states = State.all
+    respond_to do |format|
+      format.json { render json: @states }
+    end
+  end
+
+  def show
+    @state = State.find_by_slug(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @state }
+    end
   end
 
   private
 
   def state_params
     params.require(:state).permit(
-      :name
+      :name,
+      :slug,
       )
   end
 
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219173831) do
+ActiveRecord::Schema.define(version: 20150220153023) do
 
   create_table "attractions", force: :cascade do |t|
     t.string   "name",         limit: 255
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20150219173831) do
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "slug",       limit: 255
   end
 
 end
