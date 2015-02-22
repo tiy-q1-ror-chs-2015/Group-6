5c69314f8bf1805d304d6071771f455edb375c8a
courtland <courtlandgaba128@gmail.com>
2015-02-22 12:40:01 -0500

    client side updates

---
+++ client/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    page.init();
+});
+
+
+var page = {
+    init: function () {
+        page.initEvents();
+    },
+    initStyling: function () {
+    },
+    initEvents: function () {
+        $('.california').on('click', function(event){
+            event.preventDefault();
+            $('.bodyy1').addClass("hide");
+            console.log("hello");
+        })
+    }
+}
